588823b07168bd2d23b712f0ef4ff3666cc8a09d
ArmyNicolasG <ngaleanor06@gmail.com>
2023-06-14 11:22:28 -0500

    0.1.2

--- server/backend/app.js
+++ server/backend/app.js
@@ -8,7 +8,21 @@ app.get('/', (req, res) => {
     //Here, dynamic HTML page will be sent to front-end each time someone accesses the app, this is the main route.
 });
 
-app.
+// Providers
+app.post('/new-provider', (req, res) => {
+
+    const providerInfo = {
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email
+    }
+
+    if((providerInfo.name.length > 64 || providerInfo.name == "")
+        || (providerInfo.phone.length > 13 || providerInfo.phone == undefined)
+        || (isNaN(parseInt(provider.phone)) == true ) 
+    ){ res.status(406).send(); }
+
+});
 
 app.listen(process.env.HTTP_SERVER_PORT, () => {
     console.log(`Listening at ${process.env.HTTP_SERVER_PORT} port.`);
--- server/backend/database.js
+++ server/backend/database.js
@@ -2,18 +2,13 @@ const MYSQL = require('mysql');
 require('dotenv').config();
 
 const connection = MYSQL.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected!");
 });
-
-const functions = {
-
-    
-
-}
